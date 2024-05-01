149271b52fb35aa153cefdbf8e47191299c9daa8
Jonathan Becker <beckerjonphd@gmail.com>
2024-05-01 00:18:28 +0000

    Tweaking pause between questions

--- Stan/app.py
+++ Stan/app.py
@@ -1,6 +1,6 @@
 from shiny.express import input, render, ui
 import utils
-import asyncio
+import time
 
 ui.panel_title("Compliance Bot!")
 ui.input_text("message", "Ask Compliance Bot a question", 
@@ -10,7 +10,7 @@ ui.input_text("message", "Ask Compliance Bot a question",
 @render.ui
 def result():
     x=input.message()
-    asyncio.sleep(5)
+    time.sleep(2)
     print(f"The input message is {x}")
     the_answer = utils.query_data(query_text=x)
 
