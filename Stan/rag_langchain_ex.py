301236e42748610e00ac5529fff4570c63725c88
Stanly Gomes <stanlygomes916@gmail.com>
2024-05-01 20:28:20 +0000

    another commit

--- Stan/rag_langchain_ex.py
+++ Stan/rag_langchain_ex.py
@@ -1,13 +1,18 @@
 import utils
 import streamlit as st
 
+def fbcb(feedback):
+    message_id = len(st.session_state.chat_history) - 1
+    if message_id >= 0:
+        st.session_state.feedback_history.append(feedback)
+
 # Variables
 model_types = {'gpt3.5': 'gpt-3.5-turbo', 'gpt4': 'gpt-4',
                'claude-sonnet': 'claude-3-sonnet-20240229', 'claude-opus': 'claude-3-opus-20240229',
                'claude-haiku': 'claude-3-haiku-20240307'}
 
 # Streamlit ChatBot UI Setup
-st.title('Ask ComplyBot')
+st.title('Ask Wolfie')
 prompt = st.chat_input('Provide your prompt here')
 # Setup message history variable to hold old messages during session
 if 'messages' not in st.session_state:
@@ -21,10 +26,15 @@ if prompt:
     # Store provided prompt to message history variable
     st.session_state.messages.append({'role':'user', 'content':prompt})
     # Send prompt to LLM
-    response = st.chat_message('assistant').write_stream(utils.llm_rag_predict(prompt, st.session_state.messages, model_types['gpt3.5']))
+    # response, sources = st.write_stream(utils.llm_rag_predict(prompt, st.session_state.messages, 'Mexico', model_types['claude-opus']))
     # st.chat_message('assistant').write_stream(response)
-    # response = utils.llm_rag_predict(prompt, st.session_state.chat_history, model_types['claude-sonnet'])
-    # st.chat_message('assistant').markdown(response)
+    response = utils.llm_rag_predict(prompt, st.session_state.messages, 'Mexico', model_types['claude-sonnet'])
+    st.chat_message('assistant').markdown(response)
+    cols = st.columns([0.1, 1, 1, 6])
+    with cols[1]:
+        st.button(':thumbsup:', on_click=None, args=('Positive',), key='thumbsup')
+    with cols[2]:
+        st.button(':thumbsdown:', on_click=None, args=('Negative',), key='thumbsdown')
     st.session_state.messages.append(
-        {'role':'assistant', 'content':response}
+        {'role':'assistant', 'content':response.content}
     )
--- Stan/utils.py
+++ Stan/utils.py
@@ -12,6 +12,8 @@ from dataclasses import dataclass
 import os
 import shutil
 import json
+import pycountry
+import streamlit as st
 
 # Get OPEN_AI_KEY
 openai_keys = json.load(open('c:/shared/content/config/api-keys/hackathon_openai_keys.json'))
@@ -71,9 +73,11 @@ Answer the question based only on the following context:
 ---
 
 Answer the question based on the above context: {question}
+
+Include policy data that is specific to {location}
 """
 
-def query_data(question, model_type):
+def query_data(question, country, model_type):
     # # Create CLI.
     # parser = argparse.ArgumentParser()
     # parser.add_argument("query_text", type=str, help="The query text.")
@@ -86,14 +90,14 @@ def query_data(question, model_type):
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    results = db.similarity_search_with_relevance_scores(f'{query_text} Include information specific to {country}', k=3)
     if len(results) == 0 or results[0][1] < 0.7:
         print(f"Unable to find matching results.")
         return
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
+    prompt = prompt_template.format(context=context_text, question=query_text, location=country)
     print(prompt)
 
     # Select model
@@ -110,29 +114,56 @@ def query_data(question, model_type):
     print(formatted_response)
     return response_text
 
-def llm_rag_predict(question, chat_history, model_type):
-    # # Create CLI.
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("query_text", type=str, help="The query text.")
-    # args = parser.parse_args()
-    # query_text = args.query_text
-    query_text = question
+def get_countries():
+    countries = list(pycountry.countries)
+    country_names = [country.name for country in countries]
+    return country_names
+
+def contains_country(string):
+    for country in get_countries():
+        if country.lower() in string.lower():
+            return True, country
+    return False, None
+
+def fbcb(feedback):
+    message_id = len(st.session_state.chat_history) - 1
+    if message_id >= 0:
+        st.session_state.feedback_history.append(feedback)
+
+def llm_rag_predict(user_query, chat_history, country, model_type):
+    template = """
+    Answer the question based only on the following context:
+
+    {context}
+
+    ---
+
+    Answer this question based on the above context: {user_question}
+
+    Include policy data that is specific to {location}.
+    Mention that user is based in {location}.
+    """
 
     # Prepare the DB.
     embedding_function = OpenAIEmbeddings()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    question_has_country, country_found = contains_country(user_query) 
+    if question_has_country:
+        sel_country = country_found
+        relevance_prompt = f'{user_query} Include non region-specific policies as well'
+    else:
+        sel_country = country
+        relevance_prompt = f'{user_query} Include not region-specific policies and policies specific to {country}'
+    results = db.similarity_search_with_relevance_scores(user_query, k=3)
     if len(results) == 0 or results[0][1] < 0.7:
         print(f"Unable to find matching results.")
         return
 
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
-    prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
 
+    prompt = ChatPromptTemplate.from_template(template)
     # Select model
     if model_type.startswith('gpt'):
         model = ChatOpenAI(model=model_type)
@@ -140,13 +171,22 @@ def llm_rag_predict(question, chat_history, model_type):
         model = ChatAnthropic(model=model_type)
     else:
         print("Not a valid model or logic hasn't been implemented yet")
-
     chain = prompt | model | StrOutputParser()
 
-    return chain.stream({
-        "chat_history": chat_history,
-        "user_question": query_text
-    })
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    # print(formatted_response)
+    return formatted_response
+
+    # sources = [doc.metadata.get("source", None) for doc, _score in results]
+    # return chain.stream({
+    #     # "chat_history": chat_history,
+    #     "user_question": user_query,
+    #     "context": context_text,
+    #     "location": sel_country
+    # }), sources
 
     # response_text = model.invoke(prompt)
 
