b6942130858a6e629865816b1139eb8f3c0074d9
Stanly Gomes <stanlygomes916@gmail.com>
2024-05-01 05:09:42 +0000

    streamlit app initial setup

--- Stan/rag_langchain_ex.py
+++ Stan/rag_langchain_ex.py
@@ -1,3 +1,28 @@
 import utils
+import streamlit as st
 
-utils.query_data()
+# Variables
+model_types = {'gpt3.5': 'gpt-3.5-turbo', 'gpt4': 'gpt-4',
+               'claude-sonnet': 'claude-3-sonnet-20240229', 'claude-opus': 'claude-3-opus-20240229',
+               'claude-haiku': 'claude-3-haiku-20240307'}
+
+# Streamlit ChatBot UI Setup
+st.title('Ask ComplyBot')
+prompt = st.chat_input('Provide your prompt here')
+# Setup message history variable to hold old messages during session
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    st.chat_message(message['role']).markdown(message['content'])
+
+if prompt:
+    st.chat_message('user').markdown(prompt)
+    # Store provided prompt to message history variable
+    st.session_state.messages.append({'role':'user', 'content':prompt})
+    # Send prompt to LLM
+    response = utils.llm_rag_predict(prompt, model_types['claude-opus'])
+    st.chat_message('assistant').markdown(response)
+    st.session_state.messages.append(
+        {'role':'assistant', 'content':response}
+    )
--- Stan/utils.py
+++ Stan/utils.py
@@ -4,7 +4,8 @@ from langchain.schema import Document
 from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_anthropic import ChatAnthropic
 import argparse
 from dataclasses import dataclass
 import os
@@ -13,7 +14,9 @@ import json
 
 # Get OPEN_AI_KEY
 openai_keys = json.load(open('c:/shared/content/config/api-keys/hackathon_openai_keys.json'))
+claude_keys = json.load(open('c:/shared/content/config/api-keys/hackathon_claude_keys.json'))
 os.environ["OPENAI_API_KEY"] = openai_keys['team_13']
+os.environ["ANTHROPIC_API_KEY"] = claude_keys['team_5']
 
 CHROMA_PATH = "C:/ProgramData/UserDataFolders/S-1-5-21-1271617331-789234398-1144656980-1030/Documents/chroma_compliance"
 DATA_PATH = "data/compliance"
@@ -32,8 +35,8 @@ def load_documents():
 
 def split_text(documents: list[Document]):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=300,
-        chunk_overlap=100,
+        chunk_size=600,
+        chunk_overlap=200,
         length_function=len,
         add_start_index=True,
     )
@@ -69,12 +72,13 @@ Answer the question based only on the following context:
 Answer the question based on the above context: {question}
 """
 
-def query_data():
-    # Create CLI.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
-    query_text = args.query_text
+def query_data(question, model_type):
+    # # Create CLI.
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("query_text", type=str, help="The query text.")
+    # args = parser.parse_args()
+    # query_text = args.query_text
+    query_text = question
 
     # Prepare the DB.
     embedding_function = OpenAIEmbeddings()
@@ -91,9 +95,54 @@ def query_data():
     prompt = prompt_template.format(context=context_text, question=query_text)
     print(prompt)
 
-    model = ChatOpenAI()
+    # Select model
+    if model_type.startswith('gpt'):
+        model = ChatOpenAI(model=model_type)
+    elif model_type.startswith('claude'):
+        model = ChatAnthropic(model=model_type)
+    else:
+        print("Not a valid model or logic hasn't been implemented yet")
     response_text = model.invoke(prompt)
 
     sources = [doc.metadata.get("source", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
+    return response_text
+
+def llm_rag_predict(question, model_type):
+    # # Create CLI.
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("query_text", type=str, help="The query text.")
+    # args = parser.parse_args()
+    # query_text = args.query_text
+    query_text = question
+
+    # Prepare the DB.
+    embedding_function = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_relevance_scores(query_text, k=3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print(f"Unable to find matching results.")
+        return
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+    print(prompt)
+
+    # Select model
+    if model_type.startswith('gpt'):
+        model = ChatOpenAI(model=model_type)
+    elif model_type.startswith('claude'):
+        model = ChatAnthropic(model=model_type)
+    else:
+        print("Not a valid model or logic hasn't been implemented yet")
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    # formatted_response = f"{response_text.content}\nGenerated using: {response_text.response_metadata['model_name']}\nSources: {sources}"
+    formatted_response = f"{response_text.content}\nSources: {sources}"
+    print(formatted_response)
+    return formatted_response
